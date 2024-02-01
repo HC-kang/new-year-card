f73ec3e709a5413e3993a7aee94c431bc5477995
HC-kang <weston0713@gmail.com>
2024-02-02 08:57:54 +0900

    Feat: add kakao login test

--- src/app/signIn/page.tsx
+++ src/app/signIn/page.tsx
@@ -1,5 +1,21 @@
+import { redirect } from 'next/navigation';
+
 export default function SignIn() {
+  const isSignedIn = false;
+  if (isSignedIn) {
+    redirect('/');
+  }
+
+  const kakaoRestApiKey = ''
+  const kakaoRedirectUri = ''
+  const kakaoLoginUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${kakaoRestApiKey}&redirect_uri=${kakaoRedirectUri}`
+
   return (
-    <div>SignIn</div>
+    <section>
+      <a href={kakaoLoginUrl}
+        className="inline-block bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+        Kakao로 로그인하기
+      </a>
+    </section>
   );
 }
